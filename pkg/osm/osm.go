dfd78127d20ade5b1dc3b371527753e90aefdf67
John <29466887+ockendenjo@users.noreply.github.com>
2023-07-24 22:53:56 +0100

    Setup client to fetch ways and relations

--- pkg/osm/osm.go
+++ pkg/osm/osm.go
@@ -1,1 +1,85 @@
 package osm
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+const defaultBaseUrl = "https://api.openstreetmap.org/api/0.6"
+
+func NewClient() *OSMClient {
+	httpClient := http.Client{Timeout: time.Duration(3) * time.Second}
+	return &OSMClient{httpClient: httpClient, baseUrl: defaultBaseUrl}
+}
+
+type OSMClient struct {
+	httpClient http.Client
+	baseUrl    string
+}
+
+func (c *OSMClient) WithBaseUrl(baseUrl string) *OSMClient {
+	c.baseUrl = baseUrl
+	return c
+}
+
+func (c *OSMClient) GetRelation(ctx context.Context, relationId int64) (Relation, error) {
+	url := fmt.Sprintf("%s/relation/%d.json", c.baseUrl, relationId)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return Relation{}, err
+	}
+
+	response, err := c.httpClient.Do(req)
+	if err != nil {
+		return Relation{}, err
+	}
+
+	if response.StatusCode != 200 {
+		return Relation{}, fmt.Errorf("HTTP status code %d", response.StatusCode)
+	}
+
+	bytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return Relation{}, err
+	}
+
+	var relation Relation
+	err = json.Unmarshal(bytes, &relation)
+	if err != nil {
+		return Relation{}, err
+	}
+	return relation, nil
+}
+
+func (c *OSMClient) GetWay(ctx context.Context, wayId int64) (Way, error) {
+	url := fmt.Sprintf("%s/way/%d.json", c.baseUrl, wayId)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return Way{}, err
+	}
+
+	response, err := c.httpClient.Do(req)
+	if err != nil {
+		return Way{}, err
+	}
+
+	if response.StatusCode != 200 {
+		return Way{}, fmt.Errorf("HTTP status code %d", response.StatusCode)
+	}
+
+	bytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return Way{}, err
+	}
+
+	var way Way
+	err = json.Unmarshal(bytes, &way)
+	if err != nil {
+		return Way{}, err
+	}
+	return way, nil
+}
---
+++ pkg/osm/osm_test.go
@@ -0,0 +1,118 @@
+package osm
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func Test_getRelation(t *testing.T) {
+	bytes, err := os.ReadFile("testdata/relation.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testcases := []struct {
+		name      string
+		handlerFn func(t *testing.T) func(w http.ResponseWriter, r *http.Request)
+		checkFn   func(t *testing.T, r Relation, err error)
+	}{
+		{
+			name: "HTTP 200",
+			handlerFn: func(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+				return func(w http.ResponseWriter, r *http.Request) {
+					assert.Equal(t, "/relation/3411082864.json", r.RequestURI)
+					_, err := w.Write(bytes)
+					if err != nil {
+						t.Fatal(err)
+					}
+				}
+			},
+			checkFn: func(t *testing.T, r Relation, err error) {
+				assert.Nil(t, err)
+				assert.Equal(t, int64(3411082864), r.Elements[0].ID)
+			},
+		},
+		{
+			name: "HTTP 404",
+			handlerFn: func(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+				return func(w http.ResponseWriter, r *http.Request) {
+					assert.Equal(t, "/relation/3411082864.json", r.RequestURI)
+					w.WriteHeader(404)
+				}
+			},
+			checkFn: func(t *testing.T, r Relation, err error) {
+				assert.EqualError(t, err, "HTTP status code 404")
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			handlerFn := http.HandlerFunc(tc.handlerFn(t))
+			svr := httptest.NewServer(handlerFn)
+			defer svr.Close()
+
+			client := NewClient().WithBaseUrl(svr.URL)
+			relation, err := client.GetRelation(context.Background(), 3411082864)
+			tc.checkFn(t, relation, err)
+		})
+	}
+}
+
+func Test_getWay(t *testing.T) {
+	bytes, err := os.ReadFile("testdata/way.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testcases := []struct {
+		name      string
+		handlerFn func(t *testing.T) func(w http.ResponseWriter, r *http.Request)
+		checkFn   func(t *testing.T, w Way, err error)
+	}{
+		{
+			name: "HTTP 200",
+			handlerFn: func(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+				return func(w http.ResponseWriter, r *http.Request) {
+					assert.Equal(t, "/way/2154620362.json", r.RequestURI)
+					_, err := w.Write(bytes)
+					if err != nil {
+						t.Fatal(err)
+					}
+				}
+			},
+			checkFn: func(t *testing.T, w Way, err error) {
+				assert.Nil(t, err)
+				assert.Equal(t, int64(2154620362), w.Elements[0].ID)
+			},
+		},
+		{
+			name: "HTTP 404",
+			handlerFn: func(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+				return func(w http.ResponseWriter, r *http.Request) {
+					assert.Equal(t, "/way/2154620362.json", r.RequestURI)
+					w.WriteHeader(404)
+				}
+			},
+			checkFn: func(t *testing.T, w Way, err error) {
+				assert.EqualError(t, err, "HTTP status code 404")
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			handlerFn := http.HandlerFunc(tc.handlerFn(t))
+			svr := httptest.NewServer(handlerFn)
+			defer svr.Close()
+
+			client := NewClient().WithBaseUrl(svr.URL)
+			way, err := client.GetWay(context.Background(), 2154620362)
+			tc.checkFn(t, way, err)
+		})
+	}
+}
