44fc22ccd4ca1315a5adbb36e73a38bbe8f798f9
John <29466887+ockendenjo@users.noreply.github.com>
2023-08-10 17:25:57 +0100

    Cache nodes and ways for lifetime of OSMClient

--- pkg/osm/osm.go
+++ pkg/osm/osm.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-xray-sdk-go/xray"
 	"io"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -14,12 +15,19 @@ const defaultBaseUrl = "https://api.openstreetmap.org/api/0.6"
 
 func NewClient() *OSMClient {
 	httpClient := http.Client{Timeout: time.Duration(3) * time.Second}
-	return &OSMClient{httpClient: httpClient, baseUrl: defaultBaseUrl}
+	return &OSMClient{
+		httpClient: httpClient,
+		baseUrl:    defaultBaseUrl,
+		nodeCache:  NodeCache{v: map[int64]Node{}},
+		wayCache:   WayCache{v: map[int64]Way{}},
+	}
 }
 
 type OSMClient struct {
 	httpClient http.Client
 	baseUrl    string
+	nodeCache  NodeCache
+	wayCache   WayCache
 }
 
 func (c *OSMClient) WithBaseUrl(baseUrl string) *OSMClient {
@@ -61,7 +69,17 @@ func (c *OSMClient) GetRelation(ctx context.Context, relationId int64) (Relation
 	return relation, nil
 }
 
+type WayCache struct {
+	mu sync.Mutex
+	v  map[int64]Way
+}
+
 func (c *OSMClient) GetWay(ctx context.Context, wayId int64) (Way, error) {
+	cacheWay, found := c.getCachedWay(wayId)
+	if found {
+		return cacheWay, nil
+	}
+
 	url := fmt.Sprintf("%s/way/%d.json", c.baseUrl, wayId)
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
@@ -87,10 +105,22 @@ func (c *OSMClient) GetWay(ctx context.Context, wayId int64) (Way, error) {
 	if err != nil {
 		return Way{}, err
 	}
+
+	c.cacheWay(way)
 	return way, nil
 }
 
+type NodeCache struct {
+	mu sync.Mutex
+	v  map[int64]Node
+}
+
 func (c *OSMClient) GetNode(ctx context.Context, nodeId int64) (Node, error) {
+	cacheNode, found := c.getCachedNode(nodeId)
+	if found {
+		return cacheNode, nil
+	}
+
 	url := fmt.Sprintf("%s/node/%d.json", c.baseUrl, nodeId)
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
@@ -116,5 +146,35 @@ func (c *OSMClient) GetNode(ctx context.Context, nodeId int64) (Node, error) {
 	if err != nil {
 		return Node{}, err
 	}
+
+	c.cacheNode(node)
 	return node, nil
+}
+
+func (c *OSMClient) getCachedNode(nodeId int64) (Node, bool) {
+	c.nodeCache.mu.Lock()
+	defer c.nodeCache.mu.Unlock()
+	node, found := c.nodeCache.v[nodeId]
+	return node, found
+}
+
+func (c *OSMClient) cacheNode(node Node) {
+	nodeId := node.Elements[0].ID
+	c.nodeCache.mu.Lock()
+	defer c.nodeCache.mu.Unlock()
+	c.nodeCache.v[nodeId] = node
+}
+
+func (c *OSMClient) getCachedWay(wayId int64) (Way, bool) {
+	c.wayCache.mu.Lock()
+	defer c.wayCache.mu.Unlock()
+	way, found := c.wayCache.v[wayId]
+	return way, found
+}
+
+func (c *OSMClient) cacheWay(way Way) {
+	wayId := way.Elements[0].ID
+	c.wayCache.mu.Lock()
+	defer c.wayCache.mu.Unlock()
+	c.wayCache.v[wayId] = way
 }
