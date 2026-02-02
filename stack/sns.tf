f3c1918179a21f6b319c0a80effeb05e4f5bf43a
John <29466887+ockendenjo@users.noreply.github.com>
2026-02-02 17:05:03 +0000

    Revert "Setup SNS email subscriptions (#46)"
    This reverts commit 73f83ca1600e0652a62569e9f5f4a52661b987a2.

--- stack/sns.tf
+++ stack/sns.tf
@@ -2,20 +2,6 @@ resource "aws_sns_topic" "invalid_relations" {
   name = "osmptv-${var.env}-invalid-relations"
 }
 
-resource "aws_sns_topic_subscription" "invalid_relations" {
-  for_each  = toset(var.invalid_relation_emails)
-  endpoint  = each.key
-  protocol  = "email"
-  topic_arn = aws_sns_topic.invalid_relations.arn
-}
-
 resource "aws_sns_topic" "alarms" {
   name = "osmptv-${var.env}-alarms"
-}
-
-resource "aws_sns_topic_subscription" "alarms" {
-  for_each  = toset(var.alarm_emails)
-  endpoint  = each.key
-  protocol  = "email"
-  topic_arn = aws_sns_topic.alarms.arn
 }
