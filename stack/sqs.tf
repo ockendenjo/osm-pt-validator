1314bdbb2d6cef0b51527166054f26dd5dc03cf9
John <29466887+ockendenjo@users.noreply.github.com>
2026-08-05 11:30:10 +0100

    Setup DLQ alarm

--- stack/sqs.tf
+++ stack/sqs.tf
@@ -1,8 +1,9 @@
 module "sqs_validate_route_events" {
-  source       = "github.com/ockendenjo/tfmods//sqs"
-  aws_env      = var.env
-  name         = "validate-route-events"
-  project_name = "osmptv"
+  source          = "github.com/ockendenjo/tfmods//sqs"
+  aws_env         = var.env
+  name            = "validate-route-events"
+  project_name    = "osmptv"
+  alarm_topic_arn = aws_sns_topic.alarms.arn
 }
 
 module "sqs_validate_rm_events" {
